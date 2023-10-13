9bcdee214c07551c5de6d0f897ce4e0c66239128
CyJay96 <konstantin201586@gmail.com>
2023-10-13 22:28:25 +0300

    add initial api-gateway

--- inventory-service/src/main/java/com/hoaxify/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/hoaxify/inventoryservice/controller/InventoryController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -74,6 +75,7 @@ public class InventoryController {
     }
 
     @GetMapping("/bySkuCodes")
+    @ResponseStatus(HttpStatus.OK)
     public List<InventoryResponse> findBySkuCodes(@RequestParam @NotNull List<String> skuCode) {
         return inventoryService.findBySkuCodes(skuCode);
     }
